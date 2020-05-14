15e1cbf0a4ff0882e24c132ec2a03143d74ac24a
jorge-lba <jorgeluiz.b.alegretti@gmail.com>
2020-05-14 19:39:04 -0300

    Add - Iniciando funçoes do Bot

---
+++ src/certifiedGenerator/index.js
@@ -0,0 +1,18 @@
+const Jimp = require( 'jimp' )
+
+async function certifiedGenerator ( userName = 'Name' ){
+    const font = await Jimp.loadFont( Jimp.FONT_SANS_32_BLACK )
+    const certified = await Jimp.read( './assets/img/certified.png' )
+    const areaPrint = await Jimp.read( './assets/img/areaCertified.png' )
+    const areaWidthPrint = areaPrint.bitmap.width
+
+    areaPrint.print( font, 0, 28, {
+        text: userName,
+        alignmentX: await Jimp.HORIZONTAL_ALIGN_CENTER,
+      }, areaWidthPrint )
+    certified.composite( areaPrint, 237, 422 )
+
+    certified.write( './assets/certified/' + userName + '.png')
+}
+
+module.exports = certifiedGenerator
